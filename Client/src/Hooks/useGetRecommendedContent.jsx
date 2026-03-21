baf4c4676e692d8aca3e98cd8741b4e501d56ccc
PranavThorat1432 <pranavthorat95@gmail.com>
2026-03-21 20:20:38 +0530

    useGetRecommendedContent

---
+++ Client/src/Hooks/useGetRecommendedContent.jsx
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import React, { useEffect } from 'react'
+import { serverUrl } from '../App';
+import { useDispatch, useSelector } from 'react-redux';
+import { setRecommendedContent } from '../Redux/userSlice';
+
+const useGetRecommendedContent = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const fetchRecommendedContent = async () => {
+            try {
+                const result = await axios.get(`${serverUrl}/api/user/recommendation`, {
+                    withCredentials: true
+                });
+                console.log(result.data);
+                dispatch(setRecommendedContent(result.data));
+                
+            } catch (error) {
+                console.log(error);
+                dispatch(setRecommendedContent(null));
+            }
+        }
+        fetchRecommendedContent();
+    }, []);
+}
+
+export default useGetRecommendedContent;
