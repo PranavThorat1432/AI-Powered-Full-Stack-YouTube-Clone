86b2d5e46c69af5d4a4a63ed3c29299b53fa6503
PranavThorat1432 <pranavthorat95@gmail.com>
2026-03-21 20:20:59 +0530

    useGetSubscribedData Hook

---
+++ Client/src/Hooks/useGetSubscribedData.jsx
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import React, { useEffect } from 'react'
+import { serverUrl } from '../App';
+import { useDispatch, useSelector } from 'react-redux';
+import { setSubscribedChannels, setSubscribedPlaylists, setSubscribedPosts, setSubscribedShorts, setSubscribedVideos, setUserData } from '../Redux/userSlice';
+
+const useGetSubscribedData = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const fetchSubscribedData = async () => {
+            try {
+                const result = await axios.get(`${serverUrl}/api/user/get-subscribed-data`, {
+                    withCredentials: true
+                });
+                console.log(result.data);
+                dispatch(setSubscribedChannels(result.data.subscribedChannels));
+                dispatch(setSubscribedVideos(result.data.videos));
+                dispatch(setSubscribedShorts(result.data.shorts));
+                dispatch(setSubscribedPlaylists(result.data.playlists));
+                dispatch(setSubscribedPosts(result.data.posts));
+                
+            } catch (error) {
+                console.log(error);
+                dispatch(setSubscribedChannels(null));
+                dispatch(setSubscribedVideos(null));
+                dispatch(setSubscribedShorts(null));
+                dispatch(setSubscribedPlaylists(null));
+                dispatch(setSubscribedPosts(null));
+            }
+        }
+        fetchSubscribedData();
+    }, []);
+}
+
+export default useGetSubscribedData;
