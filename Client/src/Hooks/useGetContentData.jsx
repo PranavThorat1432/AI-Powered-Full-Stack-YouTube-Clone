7bcb3d49868b13248b5ba6a2f5e5bccfcf899a8e
PranavThorat1432 <pranavthorat95@gmail.com>
2026-03-21 20:19:10 +0530

    useGetContentData Hook

---
+++ Client/src/Hooks/useGetContentData.jsx
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import React, { useEffect } from 'react'
+import { serverUrl } from '../App';
+import { useDispatch, useSelector } from 'react-redux';
+import { setAllShortsData, setAllVideosData } from '../Redux/contentSlice';
+
+const useGetContentData = () => {
+
+    const dispatch = useDispatch();
+    const {userData} = useSelector((state) => state.user);
+
+    // For Videos
+    useEffect(() => {
+        const fetchAllVideos = async () => {
+            try {
+                // Use public endpoint if user is not authenticated, otherwise use authenticated endpoint
+                const endpoint = userData ? '/api/content/get-videos' : '/api/content/videos/public';
+                const result = await axios.get(`${serverUrl}${endpoint}`, {
+                    withCredentials: true
+                });
+                console.log(result.data);
+                dispatch(setAllVideosData(result.data));
+                
+            } catch (error) {
+                console.log(error);
+                dispatch(setAllVideosData(null));
+            }
+        }
+        fetchAllVideos();
+    }, [userData]);
+
+    // For Shorts
+    useEffect(() => {
+        const fetchAllShorts = async () => {
+            try {
+                // Use public endpoint if user is not authenticated, otherwise use authenticated endpoint
+                const endpoint = userData ? '/api/content/get-shorts' : '/api/content/shorts/public';
+                const result = await axios.get(`${serverUrl}${endpoint}`, {
+                    withCredentials: true
+                });
+                console.log(result.data);
+                dispatch(setAllShortsData(result.data));
+                
+            } catch (error) {
+                console.log(error);
+                dispatch(setAllShortsData(null));
+            }
+        }
+        fetchAllShorts();
+    }, [userData]);
+}
+
+export default useGetContentData;
