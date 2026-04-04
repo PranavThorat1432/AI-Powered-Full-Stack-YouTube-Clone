3cd49325601791737097177940ace934adff8de1
PranavThorat1432 <pranavthorat95@gmail.com>
2026-04-04 12:10:17 +0530

    Server (Main File)

--- Server/server.js
+++ Server/server.js
@@ -15,6 +15,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
